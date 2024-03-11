53c4c7f67398406a0be2d40c34d09f26f2ce016a
Vladyslav Vlasenko <fl4shqq@gmail.com>
2024-03-11 16:52:24 +0200

    Update main.tf

--- modules/terraform-1ver-module/main.tf
+++ modules/terraform-1ver-module/main.tf
@@ -11,3 +11,603 @@ variable "some_input" {
 output "some_output" {
   value = null_resource.some_resource.id
 }
+
+variable "var_1" {
+  type        = string
+  description = "Example variable 1"
+  default     = "value_1"
+}
+
+variable "var_2" {
+  type        = string
+  description = "Example variable 2"
+  default     = "value_2"
+}
+
+variable "var_3" {
+  type        = string
+  description = "Example variable 3"
+  default     = "value_3"
+}
+
+variable "var_4" {
+  type        = string
+  description = "Example variable 4"
+  default     = "value_4"
+}
+
+variable "var_5" {
+  type        = string
+  description = "Example variable 5"
+  default     = "value_5"
+}
+
+variable "var_6" {
+  type        = string
+  description = "Example variable 6"
+  default     = "value_6"
+}
+
+variable "var_7" {
+  type        = string
+  description = "Example variable 7"
+  default     = "value_7"
+}
+
+variable "var_8" {
+  type        = string
+  description = "Example variable 8"
+  default     = "value_8"
+}
+
+variable "var_9" {
+  type        = string
+  description = "Example variable 9"
+  default     = "value_9"
+}
+
+variable "var_10" {
+  type        = string
+  description = "Example variable 10"
+  default     = "value_10"
+}
+
+variable "var_11" {
+  type        = string
+  description = "Example variable 11"
+  default     = "value_11"
+}
+
+variable "var_12" {
+  type        = string
+  description = "Example variable 12"
+  default     = "value_12"
+}
+
+variable "var_13" {
+  type        = string
+  description = "Example variable 13"
+  default     = "value_13"
+}
+
+variable "var_14" {
+  type        = string
+  description = "Example variable 14"
+  default     = "value_14"
+}
+
+variable "var_15" {
+  type        = string
+  description = "Example variable 15"
+  default     = "value_15"
+}
+
+variable "var_16" {
+  type        = string
+  description = "Example variable 16"
+  default     = "value_16"
+}
+
+variable "var_17" {
+  type        = string
+  description = "Example variable 17"
+  default     = "value_17"
+}
+
+variable "var_18" {
+  type        = string
+  description = "Example variable 18"
+  default     = "value_18"
+}
+
+variable "var_19" {
+  type        = string
+  description = "Example variable 19"
+  default     = "value_19"
+}
+
+variable "var_20" {
+  type        = string
+  description = "Example variable 20"
+  default     = "value_20"
+}
+
+variable "var_21" {
+  type        = string
+  description = "Example variable 21"
+  default     = "value_21"
+}
+
+variable "var_22" {
+  type        = string
+  description = "Example variable 22"
+  default     = "value_22"
+}
+
+variable "var_23" {
+  type        = string
+  description = "Example variable 23"
+  default     = "value_23"
+}
+
+variable "var_24" {
+  type        = string
+  description = "Example variable 24"
+  default     = "value_24"
+}
+
+variable "var_25" {
+  type        = string
+  description = "Example variable 25"
+  default     = "value_25"
+}
+
+variable "var_26" {
+  type        = string
+  description = "Example variable 26"
+  default     = "value_26"
+}
+
+variable "var_27" {
+  type        = string
+  description = "Example variable 27"
+  default     = "value_27"
+}
+
+variable "var_28" {
+  type        = string
+  description = "Example variable 28"
+  default     = "value_28"
+}
+
+variable "var_29" {
+  type        = string
+  description = "Example variable 29"
+  default     = "value_29"
+}
+
+variable "var_30" {
+  type        = string
+  description = "Example variable 30"
+  default     = "value_30"
+}
+
+variable "var_31" {
+  type        = string
+  description = "Example variable 31"
+  default     = "value_31"
+}
+
+variable "var_32" {
+  type        = string
+  description = "Example variable 32"
+  default     = "value_32"
+}
+
+variable "var_33" {
+  type        = string
+  description = "Example variable 33"
+  default     = "value_33"
+}
+
+variable "var_34" {
+  type        = string
+  description = "Example variable 34"
+  default     = "value_34"
+}
+
+variable "var_35" {
+  type        = string
+  description = "Example variable 35"
+  default     = "value_35"
+}
+
+variable "var_36" {
+  type        = string
+  description = "Example variable 36"
+  default     = "value_36"
+}
+
+variable "var_37" {
+  type        = string
+  description = "Example variable 37"
+  default     = "value_37"
+}
+
+variable "var_38" {
+  type        = string
+  description = "Example variable 38"
+  default     = "value_38"
+}
+
+variable "var_39" {
+  type        = string
+  description = "Example variable 39"
+  default     = "value_39"
+}
+
+variable "var_40" {
+  type        = string
+  description = "Example variable 40"
+  default     = "value_40"
+}
+
+variable "var_41" {
+  type        = string
+  description = "Example variable 41"
+  default     = "value_41"
+}
+
+variable "var_42" {
+  type        = string
+  description = "Example variable 42"
+  default     = "value_42"
+}
+
+variable "var_43" {
+  type        = string
+  description = "Example variable 43"
+  default     = "value_43"
+}
+
+variable "var_44" {
+  type        = string
+  description = "Example variable 44"
+  default     = "value_44"
+}
+
+variable "var_45" {
+  type        = string
+  description = "Example variable 45"
+  default     = "value_45"
+}
+
+variable "var_46" {
+  type        = string
+  description = "Example variable 46"
+  default     = "value_46"
+}
+
+variable "var_47" {
+  type        = string
+  description = "Example variable 47"
+  default     = "value_47"
+}
+
+variable "var_48" {
+  type        = string
+  description = "Example variable 48"
+  default     = "value_48"
+}
+
+variable "var_49" {
+  type        = string
+  description = "Example variable 49"
+  default     = "value_49"
+}
+
+variable "var_50" {
+  type        = string
+  description = "Example variable 50"
+  default     = "value_50"
+}
+
+variable "var_51" {
+  type        = string
+  description = "Example variable 51"
+  default     = "value_51"
+}
+
+variable "var_52" {
+  type        = string
+  description = "Example variable 52"
+  default     = "value_52"
+}
+
+variable "var_53" {
+  type        = string
+  description = "Example variable 53"
+  default     = "value_53"
+}
+
+variable "var_54" {
+  type        = string
+  description = "Example variable 54"
+  default     = "value_54"
+}
+
+variable "var_55" {
+  type        = string
+  description = "Example variable 55"
+  default     = "value_55"
+}
+
+variable "var_56" {
+  type        = string
+  description = "Example variable 56"
+  default     = "value_56"
+}
+
+variable "var_57" {
+  type        = string
+  description = "Example variable 57"
+  default     = "value_57"
+}
+
+variable "var_58" {
+  type        = string
+  description = "Example variable 58"
+  default     = "value_58"
+}
+
+variable "var_59" {
+  type        = string
+  description = "Example variable 59"
+  default     = "value_59"
+}
+
+variable "var_60" {
+  type        = string
+  description = "Example variable 60"
+  default     = "value_60"
+}
+
+variable "var_61" {
+  type        = string
+  description = "Example variable 61"
+  default     = "value_61"
+}
+
+variable "var_62" {
+  type        = string
+  description = "Example variable 62"
+  default     = "value_62"
+}
+
+variable "var_63" {
+  type        = string
+  description = "Example variable 63"
+  default     = "value_63"
+}
+
+variable "var_64" {
+  type        = string
+  description = "Example variable 64"
+  default     = "value_64"
+}
+
+variable "var_65" {
+  type        = string
+  description = "Example variable 65"
+  default     = "value_65"
+}
+
+variable "var_66" {
+  type        = string
+  description = "Example variable 66"
+  default     = "value_66"
+}
+
+variable "var_67" {
+  type        = string
+  description = "Example variable 67"
+  default     = "value_67"
+}
+
+variable "var_68" {
+  type        = string
+  description = "Example variable 68"
+  default     = "value_68"
+}
+
+variable "var_69" {
+  type        = string
+  description = "Example variable 69"
+  default     = "value_69"
+}
+
+variable "var_70" {
+  type        = string
+  description = "Example variable 70"
+  default     = "value_70"
+}
+
+variable "var_71" {
+  type        = string
+  description = "Example variable 71"
+  default     = "value_71"
+}
+
+variable "var_72" {
+  type        = string
+  description = "Example variable 72"
+  default     = "value_72"
+}
+
+variable "var_73" {
+  type        = string
+  description = "Example variable 73"
+  default     = "value_73"
+}
+
+variable "var_74" {
+  type        = string
+  description = "Example variable 74"
+  default     = "value_74"
+}
+
+variable "var_75" {
+  type        = string
+  description = "Example variable 75"
+  default     = "value_75"
+}
+
+variable "var_76" {
+  type        = string
+  description = "Example variable 76"
+  default     = "value_76"
+}
+
+variable "var_77" {
+  type        = string
+  description = "Example variable 77"
+  default     = "value_77"
+}
+
+variable "var_78" {
+  type        = string
+  description = "Example variable 78"
+  default     = "value_78"
+}
+
+variable "var_79" {
+  type        = string
+  description = "Example variable 79"
+  default     = "value_79"
+}
+
+variable "var_80" {
+  type        = string
+  description = "Example variable 80"
+  default     = "value_80"
+}
+
+variable "var_81" {
+  type        = string
+  description = "Example variable 81"
+  default     = "value_81"
+}
+
+variable "var_82" {
+  type        = string
+  description = "Example variable 82"
+  default     = "value_82"
+}
+
+variable "var_83" {
+  type        = string
+  description = "Example variable 83"
+  default     = "value_83"
+}
+
+variable "var_84" {
+  type        = string
+  description = "Example variable 84"
+  default     = "value_84"
+}
+
+variable "var_85" {
+  type        = string
+  description = "Example variable 85"
+  default     = "value_85"
+}
+
+variable "var_86" {
+  type        = string
+  description = "Example variable 86"
+  default     = "value_86"
+}
+
+variable "var_87" {
+  type        = string
+  description = "Example variable 87"
+  default     = "value_87"
+}
+
+variable "var_88" {
+  type        = string
+  description = "Example variable 88"
+  default     = "value_88"
+}
+
+variable "var_89" {
+  type        = string
+  description = "Example variable 89"
+  default     = "value_89"
+}
+
+variable "var_90" {
+  type        = string
+  description = "Example variable 90"
+  default     = "value_90"
+}
+
+variable "var_91" {
+  type        = string
+  description = "Example variable 91"
+  default     = "value_91"
+}
+
+variable "var_92" {
+  type        = string
+  description = "Example variable 92"
+  default     = "value_92"
+}
+
+variable "var_93" {
+  type        = string
+  description = "Example variable 93"
+  default     = "value_93"
+}
+
+variable "var_94" {
+  type        = string
+  description = "Example variable 94"
+  default     = "value_94"
+}
+
+variable "var_95" {
+  type        = string
+  description = "Example variable 95"
+  default     = "value_95"
+}
+
+variable "var_96" {
+  type        = string
+  description = "Example variable 96"
+  default     = "value_96"
+}
+
+variable "var_97" {
+  type        = string
+  description = "Example variable 97"
+  default     = "value_97"
+}
+
+variable "var_98" {
+  type        = string
+  description = "Example variable 98"
+  default     = "value_98"
+}
+
+variable "var_99" {
+  type        = string
+  description = "Example variable 99"
+  default     = "value_99"
+}
+
+variable "var_100" {
+  type        = string
+  description = "Example variable 100"
+  default     = "value_100"
+}
