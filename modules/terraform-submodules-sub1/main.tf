a5d5e57a699ff54909fd3c2bb67ebb5c105121c1
Vladyslav Vlasenko <fl4shqq@gmail.com>
2024-10-10 16:18:26 +0300

    Update main.tf

--- modules/terraform-submodules-sub1/main.tf
+++ modules/terraform-submodules-sub1/main.tf
@@ -10,12 +10,6 @@ module "terraform-submodules-sub3" {
   source = "./tf-sub-sub3"
 }
 
-module "iam_iam-account" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-account"
-  version = "5.28.0"
-  # insert the 1 required variable here
-}
-
 resource "random_pet" "pet" {
 
 }
