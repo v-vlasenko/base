9ffa0d7a05ed2ae677077ea41431ebd469addde3
Vladyslav Vlasenko <fl4shqq@gmail.com>
2026-04-30 22:51:41 +0300

    test: add variable declarations for var-set testing

--- main.tf
+++ main.tf
@@ -1,3 +1,24 @@
+variable "var1" {
+  default = ""
+}
+
+variable "var2" {
+  default = ""
+}
+
+variable "var_hcl" {
+  type    = any
+  default = null
+}
+
 resource "terraform_data" "this" {
   triggers_replace = timestamp()
+}
+
+output "var1_value" {
+  value = var.var1
+}
+
+output "var2_value" {
+  value = var.var2
 }
