62804cce119c7127951a814fa9d60c5e9346fa3d
Vladyslav Vlasenko <fl4shqq@gmail.com>
2026-04-30 20:38:15 +0300

    test(varset): add variable declarations for SCALRCORE-38095 testing

--- main.tf
+++ main.tf
@@ -1,3 +1,46 @@
+variable "tf_var" {
+  description = "Test: terraform var from set"
+  default     = "not_set"
+}
+
+variable "var1" {
+  description = "Test: precedence chain"
+  default     = "not_set"
+}
+
+variable "hcl_var" {
+  description = "Test: HCL evaluated var from set"
+  default     = "not_set"
+}
+
+variable "final_var" {
+  description = "Test: final flag var from set"
+  default     = "not_set"
+}
+
+variable "sens_var" {
+  description = "Test: sensitive var from set"
+  sensitive   = true
+  default     = "not_set"
+}
+
+variable "cascade_var" {
+  description = "Test: var from cascade set"
+  default     = "not_set"
+}
+
+variable "unlink_var" {
+  description = "Test: var from unlink set"
+  default     = "not_set"
+}
+
+output "tf_var_value"      { value = var.tf_var }
+output "var1_value"        { value = var.var1 }
+output "hcl_var_value"     { value = var.hcl_var }
+output "final_var_value"   { value = var.final_var }
+output "cascade_var_value" { value = var.cascade_var }
+output "unlink_var_value"  { value = var.unlink_var }
+
 resource "terraform_data" "this" {
   triggers_replace = timestamp()
 }
