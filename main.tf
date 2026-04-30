145088e56ab6462fd522074f548ceeafe154bff5
Vladyslav Vlasenko <fl4shqq@gmail.com>
2026-04-30 20:50:55 +0300

    test(varset): add shared_key variable for scale test

--- main.tf
+++ main.tf
@@ -34,12 +34,18 @@ variable "unlink_var" {
   default     = "not_set"
 }
 
+variable "shared_key" {
+  description = "Test: shared key across 20 sets - alphabetical winner should be bulk-set-01"
+  default     = "not_set"
+}
+
 output "tf_var_value"      { value = var.tf_var }
 output "var1_value"        { value = var.var1 }
 output "hcl_var_value"     { value = var.hcl_var }
 output "final_var_value"   { value = var.final_var }
 output "cascade_var_value" { value = var.cascade_var }
 output "unlink_var_value"  { value = var.unlink_var }
+output "shared_key_value"  { value = var.shared_key }
 
 resource "terraform_data" "this" {
   triggers_replace = timestamp()
