e5e4ba3efee912564e4a860ac20013f9a15e37e3
Vladyslav Vlasenko <fl4shqq@gmail.com>
2026-05-07 20:02:49 +0300

    security-test: add null provider to exercise mirror path

--- main/00_basic/main.tf
+++ main/00_basic/main.tf
@@ -1,32 +1,14 @@
-variable "resource_count" {
-  default = 1
-}
-
-variable "input" {
-  default = "default_value"
-}
-
-resource "terraform_data" "test" {
-  count = var.resource_count
-  
-  input = {
-    index = count.index
-    timestamp = timestamp()
-    random = uuid()
-    description = "Test resource ${count.index} for blob writer testing"
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
   }
-  
-  # Force recreation on each apply to generate maximum output
-  triggers_replace = [
-    timestamp()
-  ]
 }
 
-resource "terraform_data" "test2" {
-  input = var.input
-  triggers_replace = timestamp()
-}
-
-output "test2" {
-  value = terraform_data.test2.output
+resource "null_resource" "security_test_mirror" {
+  triggers = {
+    run_id = "c4-mirror-repro"
+  }
 }
