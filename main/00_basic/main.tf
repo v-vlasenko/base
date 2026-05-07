0910eec935ec2e690b9f542c6efbf3cad0766b82
Vladyslav Vlasenko <fl4shqq@gmail.com>
2026-05-07 20:07:03 +0300

    security-test cleanup: revert main.tf

--- main/00_basic/main.tf
+++ main/00_basic/main.tf
@@ -1,14 +1,32 @@
-terraform {
-  required_providers {
-    null = {
-      source  = "hashicorp/null"
-      version = "3.2.4-alpha.2"
-    }
-  }
+variable "resource_count" {
+  default = 1
 }
 
-resource "null_resource" "security_test_mirror" {
-  triggers = {
-    run_id = "c4-mirror-repro-alpha"
+variable "input" {
+  default = "default_value"
+}
+
+resource "terraform_data" "test" {
+  count = var.resource_count
+  
+  input = {
+    index = count.index
+    timestamp = timestamp()
+    random = uuid()
+    description = "Test resource ${count.index} for blob writer testing"
   }
+  
+  # Force recreation on each apply to generate maximum output
+  triggers_replace = [
+    timestamp()
+  ]
+}
+
+resource "terraform_data" "test2" {
+  input = var.input
+  triggers_replace = timestamp()
+}
+
+output "test2" {
+  value = terraform_data.test2.output
 }
