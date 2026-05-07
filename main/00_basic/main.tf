1a16cca4c4e544313c77d7abc463f15bea8ccf76
Vladyslav Vlasenko <fl4shqq@gmail.com>
2026-05-07 22:49:30 +0300

    Refactor terraform_data resources in main.tf
    Removed the original 'test' resource and its output, and updated 'test2' resource to use the input variable.

--- main/00_basic/main.tf
+++ main/00_basic/main.tf
@@ -6,26 +6,7 @@ variable "input" {
   default = "default_value"
 }
 
-resource "terraform_data" "test" {
-  count = var.resource_count
-
-  input = {
-    index       = count.index
-    timestamp   = timestamp()
-    random      = uuid()
-    description = "Test resource ${count.index} for blob writer testing"
-  }
-
-  triggers_replace = [
-    timestamp()
-  ]
-}
-
 resource "terraform_data" "test2" {
   input            = var.input
   triggers_replace = timestamp()
-}
-
-output "test2" {
-  value = terraform_data.test2.output
 }
