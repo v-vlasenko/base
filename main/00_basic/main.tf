9a991449dd2abc7c77497aa633bbcd95514feee7
Vladyslav Vlasenko <fl4shqq@gmail.com>
2026-05-07 20:05:51 +0300

    security-test: pin null 3.2.4-alpha.2 to force network mirror

--- main/00_basic/main.tf
+++ main/00_basic/main.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     null = {
       source  = "hashicorp/null"
-      version = "~> 3.0"
+      version = "3.2.4-alpha.2"
     }
   }
 }
 
 resource "null_resource" "security_test_mirror" {
   triggers = {
-    run_id = "c4-mirror-repro"
+    run_id = "c4-mirror-repro-alpha"
   }
 }
