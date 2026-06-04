6948eb99b84a0612586bf4a0e0ef7878839711da
Vladyslav Vlasenko <fl4shqq@gmail.com>
2026-06-04 21:54:37 +0300

    Add Terraform provider configuration and resources

--- main/00_basic/main.tf
+++ main/00_basic/main.tf
@@ -1,13 +1,18 @@
-variable "resource_count" {
-  default = 1
+terraform {
+  required_providers {
+    aws = { source = "hashicorp/aws", version = ">= 3.43.0" }
+    random = { source = "hashicorp/random" }
+  }
 }
-
-variable "input" {
-  default = "default_value"
+locals { common_tags = { Team = "platform" } }
+provider "aws" {
+  region = "us-east-1"
+  default_tags { tags = local.common_tags }
 }
-
-resource "terraform_data" "test2" {
-  input            = var.input
-  triggers_replace = timestamp()
+resource "random_pet" "n" { length = 2 }
+resource "aws_iam_role" "test" {
+  name = "scalr-aws-tags-s01-${random_pet.n.id}"
+  assume_role_policy = jsonencode({Version="2012-10-17",Statement=[{Effect="Allow",Principal={Service="ec2.amazonaws.com"},Action="sts:AssumeRole"}]})
+  tags = { Purpose = "scalr-test" }
 }
-
+# s37-trigger
