55f155dd5309557646c13d4cb6c9cd97e0e33ede
Vladyslav Vlasenko <fl4shqq@gmail.com>
2026-06-16 18:27:20 +0300

    repro: add secret_val variable for sensitive test

--- main/00_basic/main.tf
+++ main/00_basic/main.tf
@@ -6,10 +6,14 @@ variable "input" {
   default = "default_value"
 }
 
+variable "secret_val" {
+  sensitive = true
+  default   = ""
+}
+
 resource "terraform_data" "test2" {
   input            = var.input
   triggers_replace = timestamp()
 }
 
 resource "null_resource" "empty" {}
-
