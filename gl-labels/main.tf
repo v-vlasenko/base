512b8a0c53559dded9132ed14c90e29cf6e97167
Vladyslav Vlasenko <fl4shqq@gmail.com>
2026-06-24 17:47:31 +0300

    canonical observe config

--- gl-labels/main.tf
+++ gl-labels/main.tf
@@ -20,21 +20,14 @@ locals {
   }
 }
 
-# Dynamic default_labels: locals + merge() that the static HCL parser cannot evaluate.
 provider "google" {
   project        = "fluent-cyclist-443522-q4"
   region         = "us-central1"
   default_labels = merge(local.common_labels, { extra = "merged_${var.team}" })
 }
 
-# Basic resource. On a create-plan, terraform_labels = resource labels + provider default_labels,
-# so the merged (resolved user + Scalr) label set is visible in the plan diff. No API token needed.
-resource "google_storage_bucket" "observe" {
-  name                        = "scalrcore-38991-gl-labels-observe"
-  location                    = "US"
-  force_destroy               = true
-  uniform_bucket_level_access = true
-  labels = {
-    explicit = "on_resource"
-  }
+data "google_client_config" "observe" {}
+
+output "effective_default_labels" {
+  value = data.google_client_config.observe.default_labels
 }
