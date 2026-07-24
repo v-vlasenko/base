7ffa358a6157546c7429aa308ee625d95c35b9ef
Vladyslav Vlasenko <fl4shqq@gmail.com>
2026-07-24 16:52:43 +0300

    SCALRCORE-39070: S10 use SCALR_AGENT_ENV interface

--- hooks/sc39070/export_foo.sh
+++ hooks/sc39070/export_foo.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-export FOO=bar
-echo "hook1: exported FOO=bar"
+# documented interface: propagates to subsequent hooks/operations via env dump
+echo "FOO=bar" >> "$SCALR_AGENT_ENV"
+export PLAIN_EXPORT=only_in_child
+echo "hook1: wrote FOO=bar to SCALR_AGENT_ENV; PLAIN_EXPORT set in-child only"
