12a0a1eaf785bab06f6023804b8321bbe77ae632
Vladyslav Vlasenko <fl4shqq@gmail.com>
2026-07-24 16:55:28 +0300

    SCALRCORE-39070: S10 propagate TF_VAR_input to operation

--- hooks/sc39070/export_foo.sh
+++ hooks/sc39070/export_foo.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# documented interface: propagates to subsequent hooks/operations via env dump
-echo "FOO=bar" >> "$SCALR_AGENT_ENV"
-export PLAIN_EXPORT=only_in_child
-echo "hook1: wrote FOO=bar to SCALR_AGENT_ENV; PLAIN_EXPORT set in-child only"
+echo "TF_VAR_input=from_hook_env" >> "$SCALR_AGENT_ENV"
+echo "hook1: wrote TF_VAR_input=from_hook_env to SCALR_AGENT_ENV"
