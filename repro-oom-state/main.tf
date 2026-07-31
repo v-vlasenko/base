8ff89d06f38bc3ac2049a01099a0e0a01c2a8abc
Vladyslav Vlasenko <fl4shqq@gmail.com>
2026-07-31 14:42:50 +0300

    Use massive for_each to trigger OOM during plan

--- repro-oom-state/main.tf
+++ repro-oom-state/main.tf
@@ -4,8 +4,12 @@ resource "null_resource" "base" {
   }
 }
 
-data "external" "oom_trigger" {
-  program = ["python3", "${path.module}/oom.py"]
+resource "null_resource" "many" {
+  for_each = toset([for i in range(0, 500000) : "resource-${i}"])
+
+  triggers = {
+    index = each.key
+  }
 }
 
 output "state_marker" {
