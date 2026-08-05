027f156dd448c5af39e6587f1787e3f662d3659c
Vladyslav Vlasenko <fl4shqq@gmail.com>
2026-08-05 11:23:38 +0300

    Add single unit AWS test

---
+++ tg-aws-single/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
