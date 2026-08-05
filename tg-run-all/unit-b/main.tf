0d70ab3ffc243ccb2dcbed436d7a978f7a60b5e1
Vladyslav Vlasenko <fl4shqq@gmail.com>
2026-08-05 11:13:47 +0300

    Add unit-b main.tf

---
+++ tg-run-all/unit-b/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+output "region" {
+  value = data.aws_region.current.name
+}
