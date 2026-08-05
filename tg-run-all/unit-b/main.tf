7742a7624f398dab16f57fdd0bf994bf53a9b390
Vladyslav Vlasenko <v.vlasenko@scalr.com>
2026-08-05 12:59:33 +0300

    test: run-all with export_shell_variables=false only, S3 creds via get_env

--- tg-run-all/unit-b/main.tf
+++ tg-run-all/unit-b/main.tf
@@ -11,22 +11,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
-provider "aws" {
-  alias  = "secondary"
-  region = "us-east-1"
-}
-
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
-data "aws_caller_identity" "secondary" {
-  provider = aws.secondary
-}
-
 output "region" {
   value = data.aws_region.current.name
-}
-
-output "secondary_account_id" {
-  value = data.aws_caller_identity.secondary.account_id
 }
--- tg-run-all/unit-b/terragrunt.hcl
+++ tg-run-all/unit-b/terragrunt.hcl
@@ -10,9 +10,11 @@ remote_state {
   }
   config = {
     bucket         = "scalr-tg-test-state"
-    key            = "tg-run-all/unit-b/terraform.tfstate"
+    key            = "tg-run-all-no-export/unit-b/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "scalr-tg-test-locks"
+    access_key     = get_env("S3_ACCESS_KEY")
+    secret_key     = get_env("S3_SECRET_KEY")
   }
 }
